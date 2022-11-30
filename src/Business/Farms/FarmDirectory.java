40b1094ba9aaa5f4fe77c3a21c4321ab23acc568
Raaga <raaga.spandana@gmail.com>
2022-11-30 10:02:22 -0500

    updated farm directory and added details

--- src/Business/Farms/FarmDirectory.java
+++ src/Business/Farms/FarmDirectory.java
@@ -4,10 +4,45 @@
  */
 package Business.Farms;
 
+import Business.FarmManager.FarmManager;
+import java.util.ArrayList;
+
 /**
  *
  * @author raaga
  */
 public class FarmDirectory {
+    
+    private ArrayList<Farm> farmList;
+    
+    public FarmDirectory() {
+        farmList = new ArrayList();
+    }
+
+    public ArrayList<Farm> getFarmList() {
+        return farmList;
+    }
+    
+    public Boolean deleteFarm(String name) {
+        for(int i = 0; i < farmList.size(); i ++) {
+            if(farmList.get(i).getName().equals(name)) {
+                farmList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Farm createFarm(String name, FarmManager rm, String phone, String location, String Network){
+        Farm rest = new Farm();
+        rest.setName(name);        
+        rest.setPhone(phone);
+        rest.setLocation(location);
+        rest.setManagerDetails(rm);
+        rest.setNetwork(Network);
+        rest.setUserAccount(rm.getAccountDetails());
+        farmList.add(rest);
+        return rest;
+    }
     
 }
