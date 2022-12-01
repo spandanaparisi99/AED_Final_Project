261b645ffe74fbbd5494fe1f2464a16b5c4bd930
Raaga <raaga.spandana@gmail.com>
2022-11-30 20:19:15 -0500

    updated grocery manager directory

--- src/Business/GroceryManager/GroceryManagerDirectory.java
+++ src/Business/GroceryManager/GroceryManagerDirectory.java
@@ -4,10 +4,47 @@
  */
 package Business.GroceryManager;
 
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
+
 /**
  *
  * @author raaga
  */
 public class GroceryManagerDirectory {
+    private ArrayList<GroceryManager> groceryManagerList;
     
+    public GroceryManagerDirectory() {
+        groceryManagerList = new ArrayList();
+    }
+
+    public ArrayList<GroceryManager> getGroceryManagerList() {
+        return groceryManagerList;
+    }
+    
+     public void setCustomerList(ArrayList<GroceryManager> groceryManagerList) {
+        this.groceryManagerList = groceryManagerList;
+    }
+    
+    public Boolean deleteGroceryManager(String name) {
+        for(int i = 0; i < groceryManagerList.size(); i ++) {
+            if(groceryManagerList.get(i).getName().equals(name)) {
+//                system.getUserAccountDirectory().deleteUser(groceryManagerList.get(i).getAccountDetails().getUsername());
+                groceryManagerList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public GroceryManager createGroceryManager(String name, UserAccount ua, String phone, String address, String Network){
+        GroceryManager cust = new GroceryManager();
+        cust.setName(name);
+        cust.setAccountDetails(ua);
+        cust.setAddress(address);
+        cust.setPhone(phone);
+        cust.setNetwork(Network);
+        groceryManagerList.add(cust);
+        return cust;
+    }
 }
