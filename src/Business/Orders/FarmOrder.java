0e789f26518b98a92350dbb3df7dae0e6a9427f3
swapnil <bhasgauri.s@northeastern.edu>
2022-12-02 11:24:26 -0500

    Farms Order and directory class added

--- src/Business/Orders/FarmOrder.java
+++ src/Business/Orders/FarmOrder.java
@@ -9,5 +9,49 @@ package Business.Orders;
  * @author Swapnil
  */
 public class FarmOrder {
-    
+    private String item;
+    private String price;
+    private String date;
+    private String time;
+    private String total;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
