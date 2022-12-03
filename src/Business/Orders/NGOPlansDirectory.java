43f15447fd576ca604338cb62ce5ca19070b02d4
swapnil <bhasgauri.s@northeastern.edu>
2022-12-03 18:25:55 -0500

    NGO Plans Order and directory class added

--- src/Business/Orders/NGOPlansDirectory.java
+++ src/Business/Orders/NGOPlansDirectory.java
@@ -4,10 +4,120 @@
  */
 package Business.Orders;
 
+import Business.NGO.NGO;
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
 /**
  *
  * @author Swapnil
  */
 public class NGOPlansDirectory {
+    private ArrayList<NGOPlanOrder> orderList;
+    private String status;
+    private int id;
+    private static int count = 1;
+    private Boolean isAccept;
+    private String comment;    
+    private UserAccount customerDetails;
+    private NGO ngo;
+
+    public ArrayList<NGOPlanOrder> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<NGOPlanOrder> orderList) {
+        this.orderList = orderList;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        NGOPlansDirectory.count = count;
+    }
+
+    public Boolean getIsAccept() {
+        return isAccept;
+    }
+
+    public void setIsAccept(Boolean isAccept) {
+        this.isAccept = isAccept;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public UserAccount getCustomerDetails() {
+        return customerDetails;
+    }
+
+    public void setCustomerDetails(UserAccount customerDetails) {
+        this.customerDetails = customerDetails;
+    }
+
+    public NGO getNGO() {
+        return ngo;
+    }
+
+    public void setNGO(NGO ngo) {
+        this.ngo = ngo;
+    }
     
+     public NGOPlansDirectory() {
+        id = count;
+        count++;
+        orderList = new ArrayList();
+        status = "Placed";
+        comment = "";        
+        isAccept = false;
+    }
+     
+
+    public void deleteOrder(String name) {
+        for (int i = 0; i < orderList.size(); i++) {
+            if (orderList.get(i).getItem().equals(name)) {
+                orderList.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void deleteOrderAll() {
+        for (int i = 0; i < orderList.size(); i++) {
+            orderList.remove(i);
+        }
+    }
+
+    public NGOPlanOrder createOrder(String item, String price, String date, NGO leg, UserAccount user) {
+        NGOPlanOrder order1 = new NGOPlanOrder();
+        order1.setItem(item);
+        order1.setPrice(price);
+        order1.setDate(date);
+        ngo = leg;
+        customerDetails = user;
+        orderList.add(order1);
+        return order1;
+    }
 }
