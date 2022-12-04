90ba6a0f4bab78e629458b394ac1b5d350e61c23
Raaga <raaga.spandana@gmail.com>
2022-12-03 20:32:28 -0500

    updated work queue class

--- src/Business/WorkQueue/WorkQueue.java
+++ src/Business/WorkQueue/WorkQueue.java
@@ -4,10 +4,22 @@
  */
 package Business.WorkQueue;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author raaga
  */
 public class WorkQueue {
+    
+     private ArrayList<WorkRequest> workRequestList;
+
+    public WorkQueue() {
+        workRequestList = new ArrayList();
+    }
+
+    public ArrayList<WorkRequest> getWorkRequestList() {
+        return workRequestList;
+    }
     
 }
